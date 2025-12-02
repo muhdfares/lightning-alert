c2942682de0d37445311986aa7777969ca0c847c
muhdfares <xfarisxss@gmail.com>
2025-12-03 00:13:20 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,6 @@
-
+// Add this to your window.onload function or at the end of the script:
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(registration => console.log('Service Worker Registered:', registration))
+    .catch(error => console.error('Service Worker Registration Failed:', error));
+}
